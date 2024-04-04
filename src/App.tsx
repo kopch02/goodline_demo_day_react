de92dfd3cbd05bf182e560b437ddc9ccaec3383f
Kopch02 <kopch08@gmail.com>
2024-04-04 22:15:19 +0700

    exploreArrow

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,29 @@
 import { ExploreTrending } from './components/ExploreTrending/ExploreTrending'
 import { Banner } from './components/Banner/Banner'
 import { Footer } from './components/Footer/Footer'
+import { UnleashRightContent } from './components/UnleashRightContent/UnleashRightContent'
+import UnleashLeftContent from './components/UnleashLeftContent/UnleashLeftContent'
+import { ExploreAllArrow } from './components/ExploreAllArrow/ExploreAllArrow'
 
 const App = () => {
   return (
     <>
-      <section>
+      <section className="container">
         <ExploreTrending />
+        <ExploreAllArrow/>
+      </section>
+      <section className="container">
+        <div className="unleash_line"></div>
+        <div className="unleash_content">
+          <UnleashLeftContent />
+          <UnleashRightContent />
+        </div>
+      </section>
+      <section className="container">
+        <Banner />
       </section>
       <section>
-        <Banner/>
-      </section>
-      <section>
-        <Footer/>
+        <Footer />
       </section>
     </>
   )
--- src/components/ExploreTrending/ExploreTrending.tsx
+++ src/components/ExploreTrending/ExploreTrending.tsx
@@ -31,5 +31,6 @@ export const ExploreTrending = () => {
         ))}
       </ul>
     </div>
+
   )
 }
