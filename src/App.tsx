75febccbdd8cc0aa45a21c86c3314589aa707582
Kopch02 <kopch08@gmail.com>
2024-04-04 18:41:17 +0700

    banner

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import { ExploreTrending } from './components/ExploreTrending/ExploreTrending'
+import { Banner } from './components/Banner/Banner'
 
 const App = () => {
   return (
     <>
       <section>
         <ExploreTrending />
+      </section>
+      <section>
+        <Banner/>
       </section>
     </>
   )
