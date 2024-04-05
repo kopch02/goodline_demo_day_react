e39d5f004915bf3be8f18a6958d1a4162760f10a
Kopch02 <kopch08@gmail.com>
2024-04-05 17:14:46 +0700

    fix

--- src/components/ExploreTrending/ExploreTrending.tsx
+++ src/components/ExploreTrending/ExploreTrending.tsx
@@ -21,16 +21,44 @@ export const ExploreTrending = () => {
   }, [])
 
   return (
-    <div className={s.explore_trending}>
-      <ul className={s.trending__list}>
-        {trendingData.map((item) => (
-          <TrendingItem
-            key={item.id}
-            item={item}
+    <div>
+      <h2 className={s.explore__header}>Explore Marketplace</h2>
+      <ul className={s.explore_filters_list}>
+        <button className={s.explore_filter_item}>
+          <span>All</span>
+        </button>
+        <button className={s.explore_filter_item}>
+          <img
+            src="img/categori-filter-logo.svg"
+            alt="categori-filter-logo"
           />
-        ))}
+          <span>Category</span>
+        </button>
+        <button className={s.explore_filter_item}>
+          <img
+            src="img/collection-filter-logo.svg"
+            alt="collection-filter-logo"
+          />
+          <span>Collection</span>
+        </button>
+        <button className={s.explore_filter_item}>
+          <img
+            src="img/price-filter-logo.svg"
+            alt="price-filter-logo"
+          />
+          <span>Price</span>
+        </button>
       </ul>
+      <div className={s.explore_trending}>
+        <ul className={s.trending__list}>
+          {trendingData.map((item) => (
+            <TrendingItem
+              key={item.id}
+              item={item}
+            />
+          ))}
+        </ul>
+      </div>
     </div>
-
   )
 }
