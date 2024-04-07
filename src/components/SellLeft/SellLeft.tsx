dbfbe8bbab3c2f398cf52c8542382f5a22b30260
Kopch02 <kopch08@gmail.com>
2024-04-07 18:49:38 +0700

    sell_page

--- src/components/SellLeft/SellLeft.tsx
+++ src/components/SellLeft/SellLeft.tsx
@@ -2,61 +2,86 @@ import s from './SellLeft.module.scss'
 
 export const SellLeft = () => {
   return (
-    <form action="https://jsonplaceholder.typicode.com/posts" method="post" className={s.form}>
       <fieldset>
-        <div className={s.name}>
-          <span className={s.name_span}>Name</span>
-          <input type="text" className={s.name_input} />
+        <div className={`${s.name} ${s.input_container}`}>
+          <span className={s.input_header}>Name</span>
+          <input type="text" className={`${s.name_input} ${s.input}`} placeholder='ArtWork Name' name='name'/>
         </div>
-        <div className={s.description}>
-          <span className={s.description_span}>Description</span>
-          <input type="text" className={s.description_input} />
+        <div className={`${s.description} ${s.input_container}`}>
+          <span className={s.input_header}>Description</span>
+          <textarea className={`${s.description_input} ${s.input}`} placeholder='Enter Your Description' name='description'/>
         </div>
         <div className={s.royalty_size}>
-          <div className={s.royalty}>
-            <span className={s.royalty_span}>Royalty</span>
-            <input type="text" className={s.royalty_input} />
+          <div className={`${s.royalty} ${s.input_container}`}>
+            <span className={s.input_header}>Royalty</span>
+            <select  className={`${s.royalty_input} ${s.select}`}  name='royalty'>
+              <option value="" disabled selected hidden className={s.placeholder}>Royalty</option>
+              <option>10%</option>
+              <option>50%</option>
+              <option>100%</option>
+            </select>
           </div>
-          <div className={s.size}>
-            <span className={s.size_span}>Size</span>
-            <input type="text" className={s.size_input} />
+          <div className={`${s.size} ${s.input_container}`}>
+            <span className={s.input_header}>Size</span>
+            <input type="text" className={`${s.size_input} ${s.input}`} placeholder='Ex - 100 x 100' name='size'/>
           </div>
         </div>
-        <div className={s.tags}>
-          <span className={s.tags_span}>Tags</span>
-          <input type="text" className={s.tags_input} />
+        <div className={`${s.tags} ${s.input_container}`}>
+          <span className={s.input_header}>Tags</span>
+          <input type="text" className={`${s.tags_input} ${s.input}`} placeholder='Beautiful Castle, Monkeys ETC' name='tags'/>
         </div>
         <div className={s.price_stock}>
-          <div className={s.price}>
-            <span className={s.price_span}>Price</span>
-            <input type="text" className={s.price_input} />
+          <div className={`${s.price} ${s.input_container}`}>
+            <span className={s.input_header}>Price</span>
+            <div className={s.price_container}>
+            <select className={`${s.price_select} ${s.select}`}  name='price_type'>
+              <option value="" selected className={s.placeholder}>ETH</option>
+              <option>BTC</option>
+              <option>ETC</option>
+            </select>
+            <input type="text" className={`${s.price_input} ${s.input}`} placeholder='0.00000007 ETC' name='price' />
+            </div>
           </div>
-          <div className={s.stock}>
-            <span className={s.stock_span}>In Stock</span>
-            <input type="text" className={s.stock_input} />
+          <div className={`${s.stock} ${s.input_container}`}>
+            <span className={s.input_header}>In Stock</span>
+            <select className={`${s.stock_input} ${s.select}`}  name='stock'>
+              <option value="" selected className={s.placeholder}>001</option>
+              <option>002</option>
+            </select>
           </div>
         </div>
         <div className={s.put}>
           <div className={s.put_text}>
-            asd
+            <span className={s.input_header}>
+              Put On Sale
+            </span>
+            <span className={s.span_side}>
+              People Will Bids On Your NFT Project
+            </span>
           </div>
           <div className={s.put_check}>
-            v
+            <input type="checkbox" id="put_check" name="put_check" className={s.checkbox}/>
+            <label htmlFor="put_check" className={s.checkbox_label}></label>
           </div>
         </div>
         <div className={s.direct}>
           <div className={s.direct_text}>
-            asd
+            <span className={s.input_header}>
+              Direct Sale
+            </span>
+            <span className={s.span_side}>
+              No Bids - Only Direct Salling
+            </span>
           </div>
           <div className={s.direct_check}>
-            v
+            <input type="checkbox" id="direct_check" name="direct_check" className={s.checkbox}/>
+            <label htmlFor="direct_check" className={s.checkbox_label}></label>
           </div>
         </div>
-        <button className={s.form_btn}>
-
+        <button className={s.form_btn} type='submit'>
+          Create
         </button>
       </fieldset>
-    </form>
   )
 }
 
--- src/pages/Sell.tsx
+++ src/pages/Sell.tsx
@@ -1,12 +1,18 @@
 import s from './Sell.module.scss'
 import { SellLeft } from '../components/SellLeft/SellLeft'
+import { SellRight } from '../components/SellRight/SellRight'
 
 export const Sell = () => {
   return (
     <>
     <section className="container">
+    <form action="https://jsonplaceholder.typicode.com/posts" method="post" className={s.form}>
       <h2 className={s.sell_header}>Create Your NFT</h2>
-      <SellLeft/>
+      <div className={s.sell_container}>
+        <SellLeft/>
+        <SellRight/>
+      </div>
+      </form>
     </section>
     </>
   )
